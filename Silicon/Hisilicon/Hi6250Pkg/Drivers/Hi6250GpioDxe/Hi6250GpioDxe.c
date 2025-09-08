8c3461957eea9ebeb896bfaa8c8633f13c6dc623
Rostislav Lastochkin <remtrik@mail.ru>
2025-09-08 16:13:00 +0300

    update

--- Silicon/Hisilicon/Hi6250Pkg/Drivers/Hi6250GpioDxe/Hi6250GpioDxe.c
+++ Silicon/Hisilicon/Hi6250Pkg/Drivers/Hi6250GpioDxe/Hi6250GpioDxe.c
@@ -15,7 +15,7 @@ GPIO_CONTROLLER gGpioDevice[] = {
   //
   // { base address, gpio index, gpio count }
   //
-  //{ 0xe8a0b000, 0, 8 },    // GPIO0
+  { 0xe8a0b000, 0, 5 },    // GPIO0
   { 0xe8a0c000, 5, 8 },    // GPIO1
   { 0xe8a0d000, 13, 8 },   // GPIO2
   { 0xe8a0e000, 21, 8 },   // GPIO3
@@ -34,9 +34,9 @@ GPIO_CONTROLLER gGpioDevice[] = {
   { 0xe8a1b000, 125, 8 },  // GPIO16
   { 0xe8a1c000, 133, 8 },  // GPIO17
   { 0xe8a1d000, 51, 2 },   // GPIO18
-  //{ 0xe8a1e000, 152, 8 }   // GPIO19
-  //{ 0xe8a1f000, 152, 8 }   // GPIO20
-  //{ 0xe8a20000, 152, 8 }   // GPIO21
+  { 0xe8a1e000, 0, 0},   // GPIO19
+  { 0xe8a1f000, 0, 0 },   // GPIO20
+  { 0xe8a20000, 0, 0 },   // GPIO21
   { 0xfff0b000, 0, 6 },   // GPIO22
   { 0xfff0c000, 6, 7 },   // GPIO23
   { 0xfff0d000, 13, 6 },   // GPIO24
@@ -45,14 +45,14 @@ GPIO_CONTROLLER gGpioDevice[] = {
   { 0xfff10000, 36, 3 },   // GPIO27
   { 0xfff1d000, 19, 2 },   // GPIO28
   { 0xfff1e000, 39, 4 },   // GPIO29
-  //{ 0xfff1f000, 152, 8 },   // GPIO30
+  { 0xfff1f000, 0, 0 },   // GPIO30
 };
 
 PLATFORM_GPIO_CONTROLLER gPlatformGpioDevice = {
   //
   // { global gpio count, gpio controller counter, GPIO_CONTROLLER }
   //
-  184, 26, gGpioDevice
+  223, 31, gGpioDevice
 };
 
 EFI_STATUS
